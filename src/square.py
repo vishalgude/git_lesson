1d7559827d9157946e81df941483471618c578cf
Vishal Gude <gudevishal@gmail.com>
2018-01-22 11:51:37 -0600

    add and update documentation for square

--- src/square.py
+++ src/square.py
@@ -2,8 +2,8 @@ def square(x):
         """takes a value and returns the squared value."""
         return(x ** 2)
 def square2(x):
+    """use the * operator to calculate square"""
         return(x * x)
 print(square2(100))
 print(square(10))
-
 
