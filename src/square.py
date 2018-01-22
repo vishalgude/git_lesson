e78e857a540f600550617fc91e4fd854643ea5cf
Vishal Gude <gudevishal@gmail.com>
2018-01-22 11:38:37 -0600

    added second fi=unction to function

--- src/square.py
+++ src/square.py
@@ -1,7 +1,9 @@
 def square(x):
         """takes a value and returns the squared value."""
         return(x ** 2)
-
+def square2(x):
+        return(x * x)
+print(square2(100))
 print(square(10))
 
 
